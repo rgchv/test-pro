8cb544b7c55572be0d0b149c8b7f561ebe83781c
qwwweq <qweqwe@wqeqe>
2024-05-31 08:41:58 +0300

    эщкере

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using ConsoleApp2;
 Console.WriteLine("Hello, World!");
+
+Trash trash = new Trash();
+
+trash.AddObjectToTrash("табуретка");
+trash.AddObjectToTrash("табуsad");
+trash.ClearTrash();
+trash.AddObjectToTrash("");
+
+Console.WriteLine("UOIUEROAURPOSAOYOTYEOIWTPOIWETUOWUEROPTIOWUEOqrup]ovruqtoi,qertqeyrtyqepotqwerpothipowerhogu aeroutgisuaertweorawpet rpwegrtpwe ofdskjgusadflkhaerklhgreasgs");
