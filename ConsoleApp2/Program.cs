2cd4e4334f2fef39a4f87b9f6021a5e3306cab7f
qwwweq <qweqwe@wqeqe>
2024-05-31 08:27:13 +0300

    Revert "трэш сделал"
    This reverts commit 371b3ecc94342ba2538fcc7984fb8e8a5bd69649.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,12 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using ConsoleApp2;
-
 Console.WriteLine("Hello, World!");
-
-Trash trash = new Trash();
-
-trash.AddObjectToTrash("табуретк");
-trash.AddObjectToTrash("яблоки тухлые");
-trash.ClearTrash();
-trash.AddObjectToTrash("хихиха");
