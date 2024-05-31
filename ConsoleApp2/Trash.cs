282b13b5291cd471b5ed3a57411dce3624be1e5b
qwwweq <qweqwe@wqeqe>
2024-05-31 08:12:30 +0300

    ewrwrwer

---
+++ ConsoleApp2/Trash.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp2;
+
+public class Trash
+{
+    private List<object> _objects = new List<object>();
+
+    public void AddObjectToTrash(object item)
+    {
+        _objects.Add(item);
+    }
+    
+}
